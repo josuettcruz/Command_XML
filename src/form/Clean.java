49091a7e3d6026b9ffb2cba6372157a98d143111
Josué <josuettcruz.jttc@gmail.com>
2026-02-16 05:54:07 -0300

    Seg, 16/02/2026 - 05:53:48 -- Nesse COMMIT -- 1-2 -> Criando a classe "CLEAN" -- 2-2 -> Classe essa, que vai substituir a classe "CLEAN PAINEL 1" do pacote "XML MF"

---
+++ src/form/Clean.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package form;
+
+import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Reg;
+import model.Link;
+
+/**
+ *
+ * @author josue
+ */
+public class Clean implements Painel_1 {
+
+    @Override
+    public String Title(boolean title) {
+        return new Link(Reg.http).page(!title);
+    }
+
+    @Override
+    public String InputText(boolean user) {
+        return "";
+    }
+
+    @Override
+    public java.util.List<String> List() {
+        List<String> rm = new ArrayList();
+        rm.add("");
+        return rm;
+    }
+
+    @Override
+    public Font ListFont() {
+        return new java.awt.Font("Times New Roman",0,72);
+    }
+
+    @Override
+    public boolean ListColumn() {
+        return false;
+    }
+
+    @Override
+    public Painel_1 Adicionar(boolean button, String input) {
+        System.exit(0);
+        return this;
+    }
+
+    @Override
+    public Painel_1 Abrir(boolean button, int index, String name, String input) {
+        System.exit(0);
+        return this;
+    }
+
+    @Override
+    public Painel_1 Apagar(boolean button, int index, String name, String input) {
+        System.exit(0);
+        return this;
+    }
+    
+}
