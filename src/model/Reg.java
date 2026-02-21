a07146b22f4af8a6bc7c5070c02363df6b238b96
Josué <josuettcruz.jttc@gmail.com>
2026-02-21 07:58:08 -0300

    Sáb, 21/02/2026 - 07:57:45 -- Nesse COMMIT --> Preparando o ambiente de trabalho!

--- src/model/Reg.java
+++ src/model/Reg.java
@@ -28,7 +28,7 @@ public class Reg {
     
     public final static Data create = new Data(2025, 10, 27);
     
-    public final static Data modify = new Data(2026, 2, 20);
+    public final static Data modify = new Data(2026, 2, 21);
     
     public final static boolean java = Arq.Exist("manifest.mf");
     
