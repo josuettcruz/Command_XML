5d4772d509ecbc68ba5a707456d14e302dbffc99
Josué <josuettcruz.jttc@gmail.com>
2026-03-26 10:05:40 -0300

    Qui, 26/03/2026 -- 10:05:16 AM -- Nesse COMMIT --> 1-3 --> Fazendo a limpeza da 'classe folder_xml' do 'pacote xml_mf' -- 2-3 --> As importações foram mantidas, mesmo que não usadas; -- 3-3 --> por isso, cuidado com o aviso de função não usada que as remove, pois as mesmas serão utilizadas posteriormente!

--- src/xml_mf/folder_xml.java
+++ src/xml_mf/folder_xml.java
@@ -9,7 +9,11 @@ import java.util.ArrayList;
 import java.awt.Font;
 
 import model.Reg;
-import form.*;
+import form.controller;
+import form.Painel_1Single;
+import form.Painel_1Multiple;
+import form.Painel_2;
+import form.Domain;
 
 import form.pag1;
 import static form.pag1.*;
@@ -24,145 +28,59 @@ import static form.pag3.*;
  *
  * @author josue
  */
-public class folder_xml implements
-        Painel_1Single,
-        Painel_1Multiple,
-        Painel_2,
-        Painel_3
-    {
+public class folder_xml implements Painel_1Single, Painel_1Multiple, Painel_2{
     
-    private List<Domain> tema;
+    private final String temp = "A função ainda não está pronta!";
     
     public folder_xml(){
         
-        var max = 10;
-        
-        this.tema = new ArrayList();
-        
-        for(int a = 1; a <= max; a++){
-            
-            var par = "Item ";
-            par += Reg.Numb(a+1, max, " de ");
-            par += "!";
-            
-            this.tema.add(new Domain(a+1, par));
-            
-        }//for(int a = 1; a <= max; a++)
+        throw new UnsupportedOperationException(this.temp);
         
     }//folder_xml()
 
     @Override
-    public Font ListFont() {
-        return new java.awt.Font("Times New Roman",0,18);
-    }
-
-    @Override
     public Font FontTitle() {
-        return new java.awt.Font("Times New Roman",0,18);
+        throw new UnsupportedOperationException(this.temp);
     }
 
     @Override
-    public Font TextAreaFont() {
-        return new java.awt.Font("Times New Roman",0,18);
+    public Font ListFont() {
+        throw new UnsupportedOperationException(this.temp);
     }
 
     @Override
     public String Title(boolean title) {
-        return "Sem Título";
+        throw new UnsupportedOperationException(this.temp);
     }
 
     @Override
     public String InputText() {
-        return "";
-    }
-
-    @Override
-    public boolean ListColumn() {
-        return false;
+        throw new UnsupportedOperationException(this.temp);
     }
 
     @Override
     public List<Domain> ListMode() {
-        
-        return this.tema;
-        
+        throw new UnsupportedOperationException(this.temp);
     }
 
     @Override
-    public List<String> TextArea() {
-        
-        List<String> tem = new ArrayList();
-        
-        for(Domain d : this.tema) tem.add(d.Text(false));
-        
-        return tem;
-        
+    public boolean ListColumn() {
+        throw new UnsupportedOperationException(this.temp);
     }
 
     @Override
-    public Domain[] Mode() {
-        
-        Domain[] tem = new Domain[10];
-        
-        for(int a = 0; a < tem.length; a++){
-            
-            var par = "Item ";
-            par += Reg.Numb(a+1,tem.length," de ");
-            par += "!";
-            
-            tem[a] = new Domain(a+1,par);
-            
-        }//for(int a = 0; a < tem.length; a++)
-        
-        return tem;
-        
+    public boolean SelectionMultiple() {
+        throw new UnsupportedOperationException(this.temp);
     }
 
     @Override
-    public void Action(pag1 action, List<Domain> vol, String input){
-        controller.p1m(new folder_xml());
+    public void Action(pag1 action, List<Domain> vol, String input) {
+        throw new UnsupportedOperationException(this.temp);
     }
-
-    @Override
-    public boolean SelectionMultiple() {return false;}
 
     @Override
     public void Command(pag2 op, List<Domain> value) {
-        controller.p2(new folder_xml());
-    }
-
-    @Override
-    public boolean JComboBox() {return false;}
-
-    @Override
-    public void Painel3(
-        int key_code,
-        char key_char,
-        Domain[] menu,
-        String input,
-        List<String> text,
-        int row,
-        int col
-    )
-    {
-        
-        if(key_code == 10) controller.p3(new folder_xml());
-        
-    }
-
-    @Override
-    public void Painel3(
-        pag3 op,
-        Domain[] menu,
-        String input,
-        List<String> text,
-        int row,
-        int col
-    )
-    {
-        
-        if(op != ComboBox) controller.p3(new folder_xml());
-        
+        throw new UnsupportedOperationException(this.temp);
     }
     
 }//folder_xml
