23f9424c2f1cf9fae76358642b20132f766a4176
Josué <josuettcruz.jttc@gmail.com>
2026-04-14 15:15:50 -0300

    Ter, 14/04/2026 -- 15:15:31 -- Nesse COMMIT --> 1-3 --> Primeiro COMMIT do dia. -- 2-3 --> Atualização da data: -- 3-3 --> 'ÚLTIMA MODIFICAÇÃO'

--- src/model/Reg.java
+++ src/model/Reg.java
@@ -28,7 +28,7 @@ public class Reg {
     
     public final static Data create = new Data(2025, 10, 27);
     
-    public final static Data modify = new Data(2026, 4, 4);
+    public final static Data modify = new Data(2026, 4, 14);
     
     public final static boolean java = Arq.Dir("manifest.mf", true);
     
