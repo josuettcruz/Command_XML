294996b4e78e1a78086cb05b227332afc77b1d66
Josué <josuettcruz.jttc@gmail.com>
2026-06-04 11:21:35 -0300

    Qui, 04/06/2026 -- 11:21:19 AM -- Nesse COMMIT --> 1-2 --> Começando a trabalhar com os caracteres especiais da futura exportação para HTML -- 2-2 --> Por definição, os dados já estarão em um 'arquivo XML'

---
+++ src/xml_rw/Characters_one.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package xml_rw;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author josue
+ */
+public class Characters_one {
+    
+    private final char err = '\b';
+    private String characters;
+    private List<String> charset;
+    
+    public Characters_one(char characters, List<String> charset){
+        
+        this.characters = "" + characters;
+        this.charset = charset;
+        
+    }//Characters_one(char characters, List<String> charset)
+    
+    public Characters_one(String characters, List<String> charset){
+        
+        this.characters = characters;
+        this.charset = charset;
+        
+    }//Characters_one(String characters, List<String> charset)
+    
+    public String Code(boolean character, int position){
+        
+        var val = "";
+        
+        if(this.charset.isEmpty() && position >= 0 && position < this.charset.size()){
+            
+            if(character) val += "&";
+            val += this.charset.get(position);
+            if(character) val += ";";
+            
+        }//if(this.charset.isEmpty() && position >= 0 && position < this.char...
+        
+        return val;
+        
+    }//Code(boolean character)
+    
+    public String Code(boolean character){return this.Code(character, 0);}
+    
+    public List<String> List(boolean character){return this.charset;}
+    
+    public String Value(){return this.characters;}
+    
+    public char Val(){
+        
+        var value = this.err;
+        
+        var i = 0;
+        var loop = true;
+        
+        do{
+            
+            value = this.characters.charAt(i);
+            
+            loop = this.characters.charAt(i) == ' ';
+            
+            i++;
+            
+        }while(loop && i > 0 && i < this.characters.length());
+        
+        return value;
+        
+    }//Val()
+    
+    public boolean Valid(){return this.Val() != this.err;}
+    
+}
